6aff53c210517e94873e3f1c34a92fd0ece064ed
andela-ajamiu <abdulmujeeb.jamiu@andela.com>
2016-10-21 18:39:01 +0100

    [#132915255] feat(user): add attributes and validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  before_save { email.downcase! if email }
+
+  validates :username, presence: true, length: { maximum: 15 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
+
+  has_secure_password
+  validates :password, length: { minimum: 6 }
 end
