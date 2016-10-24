55521f2f084352362ee3de0c4be9d2a79b13d0e0
andela-ajamiu <abdulmujeeb.jamiu@andela.com>
2016-10-24 18:04:31 +0100

    [#132915255] feat(user): add logout spec to authentication controller spec

--- spec/controllers/api/v1/authentication_controller_spec.rb
+++ spec/controllers/api/v1/authentication_controller_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::AuthenticationController, type: :controller do
+  let(:user) { create(:user) }
   describe "POST #login" do
-    let(:user) { create(:user) }
     context "when user supply correct login details" do
-      it "should login the user" do
-        post :login, params: { username: user.username, password: "validpass" }
+      it "logs in the user" do
+        post :login, username: user.username, password: "validpass"
 
         expect(response).to have_http_status(:success)
       end
     end
 
     context "when user supply invalid login details" do
-      it "should login the user" do
+      it "unauthorizes the user" do
         post :login, params: { username: user.username, password: "wrong" }
 
         expect(response).to have_http_status(401)
@@ -20,10 +20,24 @@ RSpec.describe Api::V1::AuthenticationController, type: :controller do
     end
   end
 
-  # describe "GET #logout" do
-  #   it "returns http success" do
-  #     get :logout
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET #logout" do
+    before { post :login, username: user.username, password: "validpass" }
+
+    context "when user supply a valid token header" do
+      it "logs the user out and destroys the users token" do
+        get :logout, nil, token: user.token
+        user.reload
+
+        expect(user.token).to be_nil
+      end
+    end
+
+    context "when user supply an invalid token header" do
+      it "does not logout the user" do
+        get :logout, nil, token: "3453s.dewew"
+
+        expect(user.token).not_to be_nil
+      end
+    end
+  end
 end
