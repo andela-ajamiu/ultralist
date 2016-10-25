55864f59d69521fa7e7fce1e5f6027c483d734b6
andela-ajamiu <abdulmujeeb.jamiu@andela.com>
2016-10-25 01:33:14 +0100

    [#133067761] feat(bucketlist): write model specs for bucketlist

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :bucketlists
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161023204049) do
+ActiveRecord::Schema.define(version: 20161025002251) do
+
+  create_table "bucketlists", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bucketlists_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
